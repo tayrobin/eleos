f83613b379917ac27551a39a1e3e282633bfc7d7
Taylor Robinson <taylor@appbackr.com>
2016-12-12 00:44:52 -0800

    password and login cleanup

--- eleos/urls.py
+++ eleos/urls.py
@@ -6,10 +6,11 @@ import eleos_core.views
 from eleos_core.forms import LoginForm
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-    url(r'^core/$', eleos_core.views.index, name='core_index'),
-    url(r'^integrations/$', eleos_core.views.listUserIntegrations, name='integrations'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',                  TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$',            auth_views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^accounts/login/$',   auth_views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$',           auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^core/$',             eleos_core.views.index, name='core_index'),
+    url(r'^integrations/$',     eleos_core.views.listUserIntegrations, name='integrations'),
+    url(r'^admin/',             include(admin.site.urls)),
 ]
