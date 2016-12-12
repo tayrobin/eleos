8f621267948ec7eebf236921fbc5ec478ed00d4c
Taylor Robinson <taylor@appbackr.com>
2016-12-12 13:40:03 -0800

    Module and Payload models

--- eleos_core/admin.py
+++ eleos_core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Integration, UserIntegrationLink
+from .models import *
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -20,3 +20,5 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Integration)
 admin.site.register(UserIntegrationLink)
+admin.site.register(Module)
+admin.site.register(Payload)
--- eleos_core/models.py
+++ eleos_core/models.py
@@ -6,16 +6,40 @@ from django.contrib.auth.models import User
 
 
 class Integration(models.Model):
-    name = models.CharField(max_length=200)
-    auth_url = models.CharField(max_length=200, default=None)
+    """"A service that provides information on the User's daily schedule."""
+    name = models.CharField(max_length=200, null=False)
+    auth_url = models.TextField(null=True)
 
     def __str__(self):
-        return "%s" % self.name
+        return self.name
 
 
 class UserIntegrationLink(models.Model):
+    """"A connection between a User and an Integration that serves as is_active."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     integration = models.OneToOneField(Integration, on_delete=models.CASCADE)
+    auth_token = models.TextField(null=True)
 
     def __str__(self):
         return "%s <--> %s" % (self.user.username, self.integration.name)
+
+
+class Payload(models.Model):
+    """"What gets delivered to the User in a Moment."""
+    name = models.CharField(max_length=200, null=False)
+    length = models.DurationField("The expected length of time Payload will fill.", null=True)
+    deliverable = models.TextField(null=True)
+
+    def __str__(self):
+        return "[%s] %s" % (self.length, self.name)
+
+
+class Module(models.Model):
+    """"A grouping of Integrations that deliver a Payload."""
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True)
+    required_integrations = models.ManyToManyField(Integration, blank=True)
+    possible_payloads = models.ManyToManyField(Payload, blank=True)
+
+    def __str__(self):
+        return self.name
