634e0455e1baafcdfae29ba36f8e422e83198483
Taylor Robinson <taylor@appbackr.com>
2016-12-11 18:50:15 -0800

    integrations url

--- eleos_core/views.py
+++ eleos_core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the eleos_core index.")
+
+
+@login_required(login_url="login/")
+def listUserIntegrations(request):
+    return render(request,"integrations.html")
