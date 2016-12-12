6bd4b19919e1770fc3f8c7bb3a2b7a69c0e8bb2a
Taylor Robinson <taylor@appbackr.com>
2016-12-11 19:33:21 -0800

    correct staticfiles url

--- eleos/settings.py
+++ eleos/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'staticfiles'
+STATIC_URL = os.path.join(BASE_DIR, "staticfiles/")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = '/login/'
