60a408f00eb0fa29c166f8df0b5543d6eaacb300
Taylor Robinson <taylor@appbackr.com>
2016-12-11 19:35:10 -0800

    identical static root

--- eleos/settings.py
+++ eleos/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = '/login/'
