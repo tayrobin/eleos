8f78751ee30362eb05e2cd19f2652682b2ab2ef2
Taylor Robinson <taylor@appbackr.com>
2016-12-13 13:49:49 -0800

    stringify url params in oauth cycle

--- eleos_core/views.py
+++ eleos_core/views.py
@@ -37,6 +37,27 @@ def foursquareCheckin(request):
 
     data = request.POST
     print data
+    """
+    {u'checkin': [u'{"id":"584fb810dad26340511f798c","createdAt":1481619472,"type":"checkin",
+    "timeZone":"America\\/Los_Angeles","timeZoneOffset":-480,"user":{"id":"147283036",
+    "firstName":"Taylor","lastName":"Robinson","gender":"male","relationship":"self",
+    "photo":"https:\\/\\/irs3.4sqi.net\\/img\\/user\\/110\\/147283036-QD54DS4RIHBQWNU2.jpg"},
+    "venue":{"id":"4f6c317de4b08a0bb8f4e65a","name":"Silicon Valley","contact":{},
+    "location":{"lat":37.37433343892376,"lng":-121.9569512394378,"labeledLatLngs":[{"label":"display",
+    "lat":37.37433343892376,"lng":-121.9569512394378}],"postalCode":"95051","cc":"US",
+    "city":"Santa Clara","state":"CA","country":"United States","formattedAddress":["Santa Clara, CA 95051"]},
+    "categories":[{"id":"4bf58dd8d48988d162941735","name":"Other Great Outdoors",
+    "pluralName":"Other Great Outdoors","shortName":"Other Outdoors",
+    "icon":"https:\\/\\/ss3.4sqi.net\\/img\\/categories\\/parks_outdoors\\/default.png",
+    "parents":["Outdoors & Recreation"],"primary":true}],"verified":false,"stats":{"checkinsCount":3114,
+    "usersCount":1224,"tipCount":4},"venueRatingBlacklisted":true,"beenHere":{"lastCheckinExpiredAt":0}}}'],
+    u'secret': [u'0BJPRG1FEG02NBS10SFLZDLW14LGHHZHFNILLM2TEGUXIVD0'], u'user': [u'{"id":"147283036",
+    "firstName":"Taylor","lastName":"Robinson","gender":"male","relationship":"self",
+    "photo":"https:\\/\\/irs3.4sqi.net\\/img\\/user\\/110\\/147283036-QD54DS4RIHBQWNU2.jpg",
+    "tips":{"count":0},"lists":{"groups":[{"type":"created","count":2,"items":[]}]},
+    "homeCity":"California","bio":"","contact":{"phone":"3178094648","verifiedPhone":"true",
+    "email":"taylor.howard.robinson@gmail.com","twitter":"_t_rob"}}']}
+    """
     return HttpResponse(status=201)
 
 
@@ -48,9 +69,9 @@ def sendOAuth(request, integrationName):
         return redirect('/')
     else:
         if integration.name == 'Swarm':
-            return redirect(integration.auth_url, {"client_id":os.environ['FOURSQUARE_CLIENT_ID'],
-                                                    "response_type":"code",
-                                                    "redirect_uri":"https://eleos-core.herokuapp.com/receiveOAuth"})
+            return redirect(integration.auth_url+"?"+"client_id="+os.environ['FOURSQUARE_CLIENT_ID']+
+                                                    "&"+"response_type="+"code"+
+                                                    "&"+"redirect_uri="+"https://eleos-core.herokuapp.com/receiveOAuth")
         else:
             return redirect(integration.auth_url) # ++ params
 
@@ -76,6 +97,7 @@ def receiveOAuth(request):
     integration.users.add(request.user)
 
     # Store access_token in DB
+    print request.user.username, "Swarm", access_token
 
     # send back to integrations
     return redirect('/integrations')
