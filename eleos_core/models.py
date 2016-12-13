e2b7ed78c78515a9080b216b6fd0c93955daa9eb
Taylor Robinson <taylor@appbackr.com>
2016-12-12 19:20:36 -0800

    descriptions and images on most models

--- eleos_core/models.py
+++ eleos_core/models.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 class Integration(models.Model):
     """"A service that provides information on the User's daily schedule."""
     name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True)
+    image_url = models.TextField(null=True)
     auth_url = models.TextField(null=True)
     users = models.ManyToManyField(User, blank=True)
 
@@ -18,6 +20,8 @@ class Integration(models.Model):
 class Payload(models.Model):
     """"What gets delivered to the User in a Moment."""
     name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True)
+    image_url = models.TextField(null=True)
     length = models.DurationField("The expected length of time Payload will fill.", null=True)
     deliverable = models.TextField(null=True)
 
@@ -32,8 +36,10 @@ class Module(models.Model):
     """"A grouping of Integrations that deliver a Payload."""
     name = models.CharField(max_length=200, null=False)
     description = models.TextField(null=True)
+    image_url = models.TextField(null=True)
     required_integrations = models.ManyToManyField(Integration, blank=True)
     possible_payloads = models.ManyToManyField(Payload, blank=True)
+    users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
