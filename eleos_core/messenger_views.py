527f8c45d8ad54d3f3578f1431bcca64b9d8def3
Taylor Robinson <taylor@appbackr.com>
2016-12-15 23:39:58 -0800

    import login_required && bootstrap minor fixes

--- eleos_core/messenger_views.py
+++ eleos_core/messenger_views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from .models import ActiveIntegration, Integration, Module
 
 
