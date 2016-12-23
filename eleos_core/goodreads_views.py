5ee47468d76970b9e9665b22852fd2dadab3d4ca
Taylor Robinson <taylor@appbackr.com>
2016-12-23 15:27:55 -0800

    recreate goodreads oauth object in view

--- eleos_core/goodreads_views.py
+++ eleos_core/goodreads_views.py
@@ -27,6 +27,16 @@ def receiveGoodreadsOAuth(request):
 	integration = get_object_or_404(Integration, name="Goodreads")
 	oAuthCredential = get_object_or_404(OAuthCredentials, request_token=oauth_token)
 
+	goodreads = OAuth1Service(
+		consumer_key=os.environ['GOODREADS_API_KEY'],
+		consumer_secret=os.environ['GOODREADS_CLIENT_SECRET'],
+		name='goodreads',
+		request_token_url='http://www.goodreads.com/oauth/request_token',
+		authorize_url='http://www.goodreads.com/oauth/authorize',
+		access_token_url='http://www.goodreads.com/oauth/access_token',
+		base_url='http://www.goodreads.com/'
+		)
+
 	session = goodreads.get_auth_session(oAuthCredential.request_token, oAuthCredential.request_token_secret)
 
 	# these values are what you need to save for subsequent access.
