71a4e1d99c417a33cbbef67017a1d7687f4af426
Taylor Robinson <taylor@appbackr.com>
2016-12-29 07:54:26 -0800

    django admin bootstrap

--- eleos/settings.py
+++ eleos/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'eleos_core.apps.EleosCoreConfig',
     #'eleos_core',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
