cb252d2802d9253b09bc585dd41aa5f72ce73f14
Taylor Robinson <taylor@appbackr.com>
2017-02-26 04:54:21 +0100

    parse request.POST

--- eleos_core/moment_views.py
+++ eleos_core/moment_views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse
 def createNewMoment(request):
 
     if request.method == "POST":
-        print "POST request:", request
-        data = json.loads(request.body)
+        print "POST:", request
+        print "POST request:", request.POST
+        data = json.loads(request.POST)
         print "Data:", data
     else:
         print "Not a POST"
